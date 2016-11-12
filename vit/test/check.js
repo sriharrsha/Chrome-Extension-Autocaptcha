0a41760b47b92050d6cd1c63feaa3e56c4478d68
Gopinath Nelluri <gopinathnelluri@hotmail.com>
2016-11-11 23:49:26 -0600

    v3.0

--- vit/test/check.js
+++ vit/test/check.js
@@ -5,10 +5,12 @@ function hasClass(element, cls) {
 }
 
 if(hasClass(el, "active")){
-  document.getElementById("facebook").style.display = "none";
+  document.getElementById("u_0_0").style.display = "none";
 }
 
 
 document.getElementById('u_0_5').onclick = function() {
-      document.getElementById("facebook").style.display = "none";
+      document.getElementById("u_0_0").style.display = "none";
 };
+
+document.getElementById("u_0_1").href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Fautocaptchaforvit%2Fvideos%2F1801173866782790%2F&display=popup&ref=plugin&src=share_button";
